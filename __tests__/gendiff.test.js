fa747397d7ab5eb00bfbd4a8bf793ea896bc3f5b
vzletit <munnharpa@gmail.com>
2021-06-10 13:52:24 +0300

    ESlint is glad finally

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,9 @@
-import path from 'path';
 import * as fs from 'fs';
-import gendiff from '../lib/gendiff';
-import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import * as path from 'path';
+
+import { fileURLToPath } from 'url';
+import gendiff from '../lib/gendiff';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
