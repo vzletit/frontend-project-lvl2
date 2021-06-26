29fc561b73abe73527c74fc9785cd7fc038f483e
vzletit <munnharpa@gmail.com>
2021-06-26 22:29:59 +0300

    included objects comparison support added, 'Stylish' formatter added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import genDiff from '../lib/gendiff.js';
+import stylish from '../lib/formatters/stylish.js';
 
 const program = new Command();
 
@@ -8,5 +9,5 @@ program
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
+  .action((filepath1, filepath2) => console.log(stylish(genDiff(filepath1, filepath2))))
   .parse(process.argv);
