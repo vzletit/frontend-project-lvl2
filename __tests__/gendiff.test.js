da101c832983acbd084a1e4f753c288d31b15960
vzletit <munnharpa@gmail.com>
2021-06-27 11:43:39 +0300

    Multi-level objects comparison support added. 'Stylish' formatter added

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,6 +5,8 @@ import * as path from 'path';
 import { fileURLToPath } from 'url';
 import gendiff from '../lib/gendiff';
 
+import stylish from '../lib/formatters/stylish.js';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -19,19 +21,12 @@ let file2;
 test('compare JSON', () => {
   file1 = getFixturePath('file1.json');
   file2 = getFixturePath('file2.json');
-  expect(gendiff(file1, file2)).toMatch(correctResult);
+  expect(stylish(gendiff(file1, file2))).toMatch(correctResult);
 });
 
 test('compare YAML', () => {
   file1 = getFixturePath('file1.yml');
   file2 = getFixturePath('file2.yml');
 
-  expect(gendiff(file1, file2)).toMatch(correctResult);
-});
-
-test('fail on unsupported file type', () => {
-  file1 = getFixturePath('file1.txt');
-  file2 = getFixturePath('file2.yml');
-
-  expect(gendiff(file1, file2)).toMatch('Unknown file type given. Only .json, .yml and .yaml files are supported.');
+  expect(stylish(gendiff(file1, file2))).toMatch(correctResult);
 });
